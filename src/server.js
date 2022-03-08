5c6c72e15d8f823cca5e707d4919c699b413bac8
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-03-08 22:34:30 +0000

    test3

--- src/server.js
+++ src/server.js
@@ -9,32 +9,7 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://git.heroku.com/m33-back-end.git"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors({ origin: "https://git.heroku.com/m33-back-end.git" }));
 
 app.use(userRouter, movieRouter);
 
