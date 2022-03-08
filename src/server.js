dc1f1384cf8cc3175a617a909f9dbac027510491
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-03-08 22:47:25 +0000

    test5

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const cors = require("cors");
 const userRouter = require("./user/userRoutes");
 const movieRouter = require("./movies/movieRoutes");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = 5001;
 
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({ origin: "null" }));
+app.use(cors({ origin: "https://git.heroku.com/m33-back-end.git" }));
 
 app.use(userRouter, movieRouter);
 
