b227093d0ed5d23aab8ea5a1a85ac2a0838f9193
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-03-08 22:29:39 +0000

    test2

--- src/server.js
+++ src/server.js
@@ -8,25 +8,34 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
 
+app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://git.heroku.com/m33-back-end.git"
+  );
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
 });
-app.use(Access-Control-Allow-Origin: *)
+
 app.use(userRouter, movieRouter);
 
 app.listen(port, () => {
